f54ea37385e7bc6a70ef00bad714aacb8e089504
Nendi Isharmawan <nendi@cinte.id>
2023-12-06 09:05:04 +0700

    :hammer: add readonly when confirm an set required

--- models/account_move.py
+++ models/account_move.py
@@ -6,5 +6,5 @@ from odoo import models, fields, api # Mandatory
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    jamaah_ids = fields.Many2many(comodel_name='res.partner', column1='jamaah_ids', column2="jamaah_account_id")
+    jamaah_ids = fields.Many2many(comodel_name='res.partner', column1='jamaah_ids', column2="jamaah_account_id", required=True)
    
--- models/sale_order.py
+++ models/sale_order.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api # Mandatory
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    jamaah_ids = fields.Many2many(comodel_name='res.partner', column1='jamaah_ids', column2="sale_id")
+    jamaah_ids = fields.Many2many(comodel_name='res.partner', column1='jamaah_ids', column2="sale_id", required=True)
    
     # def action_create_invoice(self):
     #     # Lakukan operasi membuat invoice seperti biasa
